9fa13502f86031a3c5bd1705323c9f27393ac23d
awescpp <awescpp@fastmail.com>
2026-05-31 14:41:49 +0800

    refactor(log): optimize log level control
    Refactor logger initialization so all module loggers share the same sinks.
    
    Configure the static log level in CMakeLists.txt to control compile-time log filtering.
    
    Keep proxypp::log::SetLevel and proxypp::log::SetAllLevels for runtime log level control of
    individual modules or all modules.

--- src/proxypp/log/log.cpp
+++ src/proxypp/log/log.cpp
@@ -1,32 +1,39 @@
 #include "log.h"
+#include <magic_enum/magic_enum.hpp>
 #include <spdlog/sinks/rotating_file_sink.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 
 namespace proxypp::log {
     // Anonymous namespace: used only inside log.cpp, hidden from external code, and not visible during linking.
     namespace {
-        std::vector<spdlog::sink_ptr> MakeSinks() {
-            auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-            console_sink->set_pattern("[%H:%M:%S.%e] [%n] [%^%l%$] [%s:%#] %v");
+        std::vector<spdlog::sink_ptr> &MakeSinks() {
+            static std::vector<spdlog::sink_ptr> sinks;
 
+            if (sinks.empty()) {
+                auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+                console_sink->set_pattern("[%H:%M:%S.%e] [%n] [%^%l%$] [%s:%#] %v");
 
-            const auto log_file = "logs/proxypp.log";
-            constexpr auto file_max_size = 1024 * 1024 * 10;
-            constexpr auto max_file_num = 5;
-            auto file_sink =
-                    std::make_shared<spdlog::sinks::rotating_file_sink_mt>(log_file, file_max_size, max_file_num);
-            file_sink->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%n] [%l] [%t] [%s:%#] %v");
+                const auto log_file = "logs/proxypp.log";
+                constexpr auto file_max_size = 1024 * 1024 * 10;
+                constexpr auto max_file_num = 5;
+                auto file_sink =
+                        std::make_shared<spdlog::sinks::rotating_file_sink_mt>(log_file, file_max_size, max_file_num);
+                file_sink->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%n] [%l] [%t] [%s:%#] %v");
 
-            return {console_sink, /* TODO: add file_sink later */};
+                sinks.push_back(console_sink);
+                // TODO: we all file_sink just later
+                //  sinks.push_back(file_sink);
+            }
+
+            return sinks;
         }
 
         std::shared_ptr<spdlog::logger> MakeLogger(Module module) {
             auto sinks = MakeSinks();
-
             std::unordered_map<Module, std::string> map = {{Module::core, "core"}, {Module::http, "http"}};
             assert(map.contains(module));
             auto logger = std::make_shared<spdlog::logger>(map[module], sinks.begin(), sinks.end());
-            logger->set_level(spdlog::level::info);
+            logger->set_level(spdlog::level::info); // default level
             logger->flush_on(spdlog::level::warn);
             spdlog::register_logger(logger);
             return logger;
@@ -46,7 +53,10 @@ namespace proxypp::log {
 
     } // namespace detail
 
-    void Init() { detail::core(); }
+    void Init() {
+        detail::core();
+        detail::http();
+    }
 
     void SetLevel(Module module, spdlog::level::level_enum level) {
         switch (module) {
@@ -59,6 +69,10 @@ namespace proxypp::log {
         }
     }
 
-    void SetAllLevels(spdlog::level::level_enum level) { detail::core()->set_level(level); }
+    void SetAllLevels(spdlog::level::level_enum level) {
+        for (auto module: magic_enum::enum_values<Module>()) {
+            SetLevel(module, level);
+        }
+    }
 
 } // namespace proxypp::log
