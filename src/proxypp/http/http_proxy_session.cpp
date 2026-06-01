b4257d2f736e7a71d04c230478707c7a6bca18a1
awescpp <awescpp@fastmail.com>
2026-06-01 20:51:01 +0800

    feat(http): implement HttpProxySession close logic

--- src/proxypp/http/http_proxy_session.cpp
+++ src/proxypp/http/http_proxy_session.cpp
@@ -8,49 +8,34 @@ namespace proxypp::http {
         client_sock_(std::move(client_sock)), remote_sock_(client_sock_.get_executor()),
         resolver_(client_sock_.get_executor()) {}
 
-    asio::awaitable<void> HttpProxySession::Run() {
+    asio::awaitable<void> HttpProxySession::Run() {}
 
-    }
+    asio::awaitable<std::optional<HttpProxySession::RequestHeader>> HttpProxySession::ParseClientRequestHeader() {}
 
-    asio::awaitable<std::optional<HttpProxySession::RequestHeader>> HttpProxySession::ParseClientRequestHeader() {
+    std::optional<HttpProxySession::RemoteInfo> HttpProxySession::ParseRemoteInfo(const RequestHeader &) {}
 
-    }
+    asio::awaitable<std::optional<tcp::resolver::results_type>> HttpProxySession::ResolveRemote(const RemoteInfo &) {}
 
-    std::optional<HttpProxySession::RemoteInfo> HttpProxySession::ParseRemoteInfo(const RequestHeader &) {
-
-    }
-
-    asio::awaitable<std::optional<tcp::resolver::results_type>> HttpProxySession::ResolveRemote(const RemoteInfo &) {
-
-    }
-
-    asio::awaitable<bool> HttpProxySession::ConnectRemote(const tcp::resolver::results_type &) {
-
-    }
+    asio::awaitable<bool> HttpProxySession::ConnectRemote(const tcp::resolver::results_type &) {}
 
     HttpProxySession::RequestHeader HttpProxySession::BuildRemoteRequestHeader(const RequestHeader &,
-                                                                               const RemoteInfo &) {
+                                                                               const RemoteInfo &) {}
 
-    }
+    asio::awaitable<bool> HttpProxySession::WriteRemoteRequestHeader(const RequestHeader &) {}
 
-    asio::awaitable<bool> HttpProxySession::WriteRemoteRequestHeader(const RequestHeader &) {
+    asio::awaitable<void> HttpProxySession::RelayClientToRemote() {}
 
-    }
+    asio::awaitable<void> HttpProxySession::RelayRemoteToClient() {}
 
-    asio::awaitable<void> HttpProxySession::RelayClientToRemote() {
-
-    }
-
-    asio::awaitable<void> HttpProxySession::RelayRemoteToClient() {
-
-    }
-
-    asio::awaitable<void> HttpProxySession::StartRelay() {
-
-    }
+    asio::awaitable<void> HttpProxySession::StartRelay() {}
 
     void HttpProxySession::Close() {
-        
+        boost::system::error_code ignored;
+        client_sock_.socket().shutdown(tcp::socket::shutdown_both, ignored);
+        client_sock_.socket().close();
+
+        remote_sock_.socket().shutdown(tcp::socket::shutdown_both, ignored);
+        remote_sock_.socket().close();
     }
 
 
