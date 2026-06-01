f5715bd34b3fdb480d64fa04b2996a040254ccea
awescpp <awescpp@fastmail.com>
2026-06-01 22:04:29 +0800

    test(http): add Boost.Url parsing cases
    Add Boost.Url test cases to verify how different HTTP request target forms are parsed.

---
+++ tests/test_boost_url.cpp
@@ -0,0 +1,31 @@
+#define BOOST_TEST_MODULE BoostUrlTest
+
+#include <boost/test/unit_test.hpp>
+#include <boost/url.hpp>
+
+BOOST_AUTO_TEST_SUITE(boost_url_test_suite)
+
+BOOST_AUTO_TEST_CASE(parse_absolute_uri_without_scheme_should_fail)
+{
+  const auto url = "httpbin.org";
+  const auto result = boost::urls::parse_absolute_uri(url);
+  BOOST_TEST(result.has_error());
+}
+
+BOOST_AUTO_TEST_CASE(parse_absolute_uri_with_port_should_return_port)
+{
+  const auto url = "http://httpbin.org:3000";
+  const auto result = boost::urls::parse_absolute_uri(url);
+  BOOST_TEST(!result.has_error());
+  BOOST_TEST(result->port() == "3000");
+}
+
+BOOST_AUTO_TEST_CASE(parse_absolute_uri_without_port_should_return_empty_port)
+{
+  const auto url = "http://httpbin.org";
+  const auto result = boost::urls::parse_absolute_uri(url);
+  BOOST_TEST(!result.has_error());
+  BOOST_TEST(result->port() == "");
+}
+
+BOOST_AUTO_TEST_SUITE_END()
