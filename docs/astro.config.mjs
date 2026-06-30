1b0cb0f587fba32bb0f56c38021a1edd1cee33f9
awescpp <awescpp@fastmail.com>
2026-06-30 20:30:36 +0800

    docs(site): initialize documentation site
    Initialize the proxy++ documentation site with the basic project structure and site configuration.

---
+++ docs/astro.config.mjs
@@ -0,0 +1,40 @@
+// @ts-check
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: 'proxy++',
+      defaultLocale: 'root',
+      locales: {
+        root: {
+          label: '简体中文',
+          lang: 'zh-CN',
+        },
+        en: { label: 'English', lang: 'en' },
+      },
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/awescpp/proxypp',
+        },
+      ],
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', slug: 'guides/example' },
+          ],
+        },
+        {
+          label: 'Reference',
+          items: [{ autogenerate: { directory: 'reference' } }],
+        },
+      ],
+    }),
+  ],
+})
